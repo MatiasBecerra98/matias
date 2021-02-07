bb0adbf5feff3f4ed43f13a56c499b3cdad0423d
Matias <ma.becerrah@alumnos.duoc.cl>
2021-02-07 07:22:08 -0300

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
+  root 'home#show'
+  get '/show', to: 'home#show'
+
+  defaults format: :json do
+    namespace :api do
+      namespace :v1 do
+        post '/gps', to: 'location#create'
+      end
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
