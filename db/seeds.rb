90ae626314d34dc843607e60dd25811728496cf0
Matias <ma.becerrah@alumnos.duoc.cl>
2021-02-07 22:52:02 -0300

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Vehicle.create(code: 'DJ-3421')
+Vehicle.create(code: 'EG-2384')
+Vehicle.create(code: 'OC-2542')
+
+Coordinate.create(
+  latitude: '-80.675475',
+  longitude: '-43.205394',
+  sent_at: DateTime.parse('2020-06-03 20:45:00'),
+  vehicle_id: Vehicle.find_by_code('OC-2542').id
+)
+
+Coordinate.create(
+  latitude: '-33.4081785',
+  longitude: '-70.5484857',
+  sent_at: DateTime.parse('2020-06-03 20:45:00'),
+  vehicle_id: Vehicle.find_by_code('EG-2384').id
+)
