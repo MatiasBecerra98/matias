ae926d820d242bb73827373d48977917909d3fcc
Matias <ma.becerrah@alumnos.duoc.cl>
2021-02-07 22:47:29 -0300

    create show method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,26 @@
+# Home controller to show cars locations
 class HomeController < ActionController::Base
   def show
-    @vehicles = Vehicle.all
+    @vehicles = []
+    Vehicle.all.each do |vehicle|
+      next if vehicle.coordinates.empty?
 
-    @geojson = @vehicles.map do |vehicle|
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [
-            vehicle.last_coordinates.latitude,
-            vehicle.last_coordinates.longitude
-          ]
-        },
+      @vehicles << vehicle
+    end
+
+    @geojson = define_geoson(@vehicles)
+  end
+
+  def define_geoson(vehicles)
+    vehicles.map do |vehicle|
+      { type: 'Feature', geometry: { type: 'Point', coordinates: [
+        vehicle.last_coordinates.latitude, vehicle.last_coordinates.longitude
+      ] },
         properties: {
           name: vehicle.code,
           'marker-color': '#00607d',
-          'marker-symbol': 'circle',
-          'marker-size': 'medium'
-        }
-      }.to_json
+          'marker-symbol': 'circle', 'marker-size': 'medium'
+        } }.to_json
     end
   end
 end
